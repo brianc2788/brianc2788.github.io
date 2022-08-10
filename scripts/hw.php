918a4d99ad3525a74f88357528d271cf0ce8cb12
brianc2788 <brianc2788@gmail.com>
2022-08-10 10:03:58 -0400

    Created a dir for scripting.

---
+++ scripts/hw.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>brianc2788's PHP Test on Github Pages</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+    <?php echo '<p>PHP is a recursive acronym that stands for "PHP Hypertext Preprocessor."</p>'; ?>
+</body>
+</html>
