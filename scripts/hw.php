5602efe500278e574ebe7a1b9b8f06402771a2e7
brianc2788 <brianc2788@gmail.com>
2022-08-14 01:51:18 -0400

    Removed extraneous html.

--- scripts/hw.php
+++ scripts/hw.php
@@ -1,9 +1,3 @@
-<html>
-<head>
-    <title>brianc2788's PHP Test on Github Pages</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-</head>
-<body>
-    <?php echo '<p>PHP is a recursive acronym that stands for "PHP Hypertext Preprocessor."</p>'; ?>
-</body>
-</html>
+<?php
+    echo '<p>PHP is a recursive acronym that stands for "PHP Hypertext Preprocessor."</p>';
+?>
