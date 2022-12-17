3b09e1dc63efcc6b7138ba10017f7c1342761648
brianc2788 <brianc2788@gmail.com>
2022-12-17 15:12:43 -0500

    Added test client-side script; print ascii table.

---
+++ js/printch.js
@@ -0,0 +1,23 @@
+// print 
+
+console.log("-- NUMBER CHARS (48-57, 0x30-0x39) --")
+// numerals (ascii value range)
+for (n = 48; n <= 57; n++) {
+	s = String.fromCharCode(n)
+	output = "Value: " + n + " - Char: " + s
+	console.log(output)
+}
+
+console.log("\n-- ALPHABET (lower, upper) --")
+for (n = 65; n <= 90; n++) {
+	sUpper = String.fromCharCode(n)
+	sLower = String.fromCharCode(n+32)
+	output = "Value: " + n + " - Upper: " + sUpper + " Lower: " + sLower
+	console.log(output)
+}
+
+//document.write("<p><b>Check your dev console</b></p>")
+p1 = document.createElement("p")
+p1.addAttribute("font-weight","900")
+p1.innerText = "Check the dev console."
+document.appendChild(p1)
