e8b1822ec1a9bd90fe636f080384d339c92a9d3c
brianc2788 <brianc2788@gmail.com>
2023-01-07 23:45:32 -0500

    Removed repo dir 'unused'.

--- unused/xml_gethdrs.js
+++
@@ -1,6 +0,0 @@
-xr = new XMLHttpRequest()
-xr.open("GET","/index.html")
-ua = navigator.userAgent.toString()
-headers = xr.getAllResponseHeaders().toString()
-document.getElementById("xml1").innerText = ua + "\n" + headers
-//document.alert("the thing finished.")
